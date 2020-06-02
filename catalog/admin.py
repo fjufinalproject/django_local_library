b2c203396d0e71e61749c5db0df8b78e3d80183c
fjufinalproject <fjufinalproject@gmail.com>
2020-06-02 23:54:45 +0800

    First version of application moved into github

---
+++ catalog/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance #將模型導入後
+#模型管理(ModelAdmin)類別 (是用來描述排版的)
+
+# Register your models here. #註冊
+#admin.site.register(Book)
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+#admin.site.register(Author)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+
+#admin.site.register(BookInstance)
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back','borrower')
+        }),
+    )
